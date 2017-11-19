a8d061b7f37d48a102f450eea25127c71a395f59
Chandan Troughia <chandantroughia.com>
2017-11-19 02:46:17 -0500

    Third Commit

--- FacebookScraper.py
+++ FacebookScraper.py
@@ -3,10 +3,10 @@ import re
 r = re.compile('alt="" aria-label="(.*?)" role="img" /></a>')
 
 temp = []
-#with open('OverheardAtRpi.txt', "r", encoding="utf-8") as f:
-    
-    
-with open('OverheardAtRpi.txt', "r", encoding="utf-8") as f:
+#with open('SourceCode.txt', "r", encoding="utf-8") as f:
+
+
+with open('SourceCode.txt', "r", encoding="utf-8") as f:
     for i in f.readlines():
         print(i)
         if(r.findall(i)):
@@ -16,6 +16,6 @@ nameList = []
 for eachList in temp:
     for names in eachList:
         nameList.append(names)
-        
+
 print(nameList)
 print(len(nameList))
